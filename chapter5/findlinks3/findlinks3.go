9eb5e883e3c91203f81e5e757c8cfed5dd7b2c60
cqping2 <cqping2@grgbanking.com>
2018-06-13 07:58:18 +0800

    update findlinks3

--- chapter5/findlinks3/findlinks3.go
+++ chapter5/findlinks3/findlinks3.go
@@ -1,3 +1,35 @@
 package main
 
+import (
+	"fmt"
+	"golang/chapter5/links"
+	"log"
+	"os"
+)
 
+func breadthFirst(f func(item string) []string, worklist []string){
+	seen := make(map[string]bool)
+	for len(worklist) > 0{
+		items := worklist
+		worklist = nil
+		for _, item := range items{
+			if !seen[item] {
+				seen[item] = true
+				worklist = append(worklist, f(item)...)
+			}
+		}
+	}
+}
+
+func craw(url string) []string{
+	fmt.Println(url)
+	list, err := links.Extract(url)
+	if err != nil{
+		log.Print(err)
+	}
+	return list
+}
+
+func main(){
+	breadthFirst(craw, os.Args[1:])
+}
